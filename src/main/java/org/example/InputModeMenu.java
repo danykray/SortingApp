57780b7cd3f4122dba95241501ec63a0ca7203b7
Delos <delosus@yandex.ru>
2026-02-19 09:36:04 +0700

    InputModeMenu-2

--- src/main/java/org/example/InputModeMenu.java
+++ src/main/java/org/example/InputModeMenu.java
@@ -1,47 +1,45 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
-import static org.example.InputMode.*;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-enum InputMode {
-    MANUAL,
-    RANDOM,
-    FILE,
-    EXIT
-}
-
-
-public class InputModeMenu{
+public class InputModeMenu {
 
     private final Scanner scanner;
+    private boolean isRunning = true;
 
     public InputModeMenu(Scanner scanner) {
         this.scanner = scanner;
     }
 
-    public InputMode chooseMode() {
-        while (true) {
+    public void start() {
+        RandomDataGenerator generator = new RandomDataGenerator();
+
+        while (isRunning) {
             printMenu();
+            String input = scanner.nextLine().trim();
 
             try {
-                int choice = scanner.nextInt();
-                scanner.nextLine();
-
+                int choice = Integer.parseInt(input);
                 switch (choice) {
-                    case 0: return InputMode.EXIT;
-                    case 1: return InputMode.MANUAL;
-                    case 2: return InputMode.RANDOM;
-                    case 3: return InputMode.FILE;
+                    case 1:
+                        manual();
+                        break;
+                    case 2:
+                        random(generator);
+                        break;
+                    case 3:
+                        file();
+                        break;
+                    case 0:
+                        isRunning = false;
+                        break;
                     default:
-                        System.out.println("Неверный выбор. Попробуйте снова.");
+                        System.out.println("Неверный выбор. Введите число от 0 до 3.");
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Нужно ввести число");
-                scanner.nextLine();
+
+            } catch (NumberFormatException e) {
+                System.out.println("Нужно ввести число.");
             }
         }
     }
@@ -55,5 +53,35 @@ public class InputModeMenu{
         0 — Выход
         """);
         System.out.print("");
+    }
+
+    private void manual() {
+        System.out.println("Пока нет");
+    }
+
+    private void random(RandomDataGenerator generator) {
+        int size = requestArraySize();
+        List<Bus> buses = generator.generate(size);
+
+        System.out.println("Результат :");
+        buses.forEach(System.out::println);
+    }
+
+    private void file() {
+        System.out.println("Пока нет");
+    }
+
+    private int requestArraySize() {
+        while (true) {
+            System.out.print("Введите количество автобусов:");
+            String input = scanner.nextLine().trim();
+            try {
+                int size = Integer.parseInt(input);
+                if (size > 0) return size;
+                else System.out.println("Число должно быть больше 0");
+            } catch (NumberFormatException e) {
+                System.out.println("Нужно ввести число");
+            }
+        }
     }
 }
