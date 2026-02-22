f752c95c8eecf0835f5ee4914a97754dd951cc4e
Niko-prog <118878076+Niko-prog@users.noreply.github.com>
2026-02-22 22:09:27 +0300

    File-input

---
+++ src/main/java/org/example/BusParser.java
@@ -0,0 +1,50 @@
+package org.example;
+
+public class BusParser {
+
+    private static final String DELIMITER_ = ";";
+
+    private BusParser() {
+
+    }
+
+    public static Bus parse(String line, int lineNumber) throws InvalidBusDataException {
+
+    String[] parts = line.split(DELIMITER_);
+
+    if (parts.length != 3) {
+        throw new InvalidBusDataException(lineNumber, line, "Ожидалось 3 поля, но получено " + parts.length);
+    }
+    
+    String number = parts[0].trim();
+    String model = parts[1].trim();
+    String mileageStr = parts[2].trim();
+
+    if (number.isEmpty()) {
+        throw new InvalidBusDataException(lineNumber, line, "Поле 'номер' пустое");
+    }
+
+    if (!number.matches("[A-Za-z0-9]+")) {
+        throw new InvalidBusDataException(lineNumber, line, "Номер должен состоять только из букв и цифр: '" + number + "'");
+    }
+
+    if (model.isEmpty()) {
+        throw new InvalidBusDataException(lineNumber, line, "Поле 'модель' пустое");
+    }
+
+    Integer mileage;
+
+        try {
+            mileage = Integer.parseInt(mileageStr);
+        } catch (NumberFormatException e) {
+            throw new InvalidBusDataException(lineNumber, line, "Пробег не является целым числом: '" + mileageStr + "'");
+        }
+
+        if (mileage < 0) {
+            throw new InvalidBusDataException(lineNumber, line, "Пробег не может быть отрицательным: " + mileage);
+        }
+
+    return new Bus(number, model, mileage);
+
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main(String[] args) {
+        System.out.println("Чтение данных об автобусах из ../buses.txt\n");
 
+        List<Bus> buses = ReadFile.readAll();
+
+        System.out.println("\nНайдено корректных записей: " + buses.size());
+
+        for (Bus bus : buses) {
+            System.out.println(bus);
+        }
     }
 }
